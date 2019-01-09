b17f161ce7c5a6a9b81234fe21eca3b924e589a8
Lynne Daniels <lynne33477@gmail.com>
2019-01-09 11:29:46 -0500

    feat(new repo): exploring end to end testing with Cypress

---
+++ tests/e2e/specs/moretests.js
@@ -0,0 +1,5 @@
+describe("Example of Another Passed Test", () => {
+  it("Passes a sample test", () => {
+    expect(true).to.equal(true);
+  });
+});
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,5 +4,20 @@ describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
     cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("Welcome");
+    cy.contains("About").click();
+    cy.url().should("include", "/about");
+  });
+});
+
+describe("Example of Failed Test", () => {
+  xit("Fails a sample test", () => {
+    expect(true).to.equal(false);
+  });
+});
+
+describe("Example of Passed Test", () => {
+  it("Passes a sample test", () => {
+    expect(true).to.equal(true);
   });
 });
